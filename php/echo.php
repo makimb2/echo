c9138b8c5db6fd558499c9642e32c7efd733fe89
makimb2 <matthewakimball@gmail.com>
2015-01-24 16:27:59 -0700

    echo.php
    echo file

--- php/echo.php
+++ php/echo.php
@@ -1,10 +1,17 @@
 <?php
+	function first()
+    {
+    $numargs = func_num_args();    
+    $arg_list = func_get_args();
+    for ($i = 0; $i < $numargs; $i++) {
+    echo "$arg_list[$i]" . "<br />\n";
 
-function echo()
-{
-  // ...
-}
+    }
+        }
 
-echo();
-echo('bla');
-echo('foo', 'bar', 'baz');
+    first();
+    first('bla');
+    first('foo','bar','baz');
+    first('happy','days','are','on','their','way');
+		
+?>
